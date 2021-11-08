29d1d40232969e8a0bc1cca468ddb874faec1145
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2021-11-08 18:54:22 -0500

    done

--- main.js
+++ main.js
@@ -1,3 +1,100 @@
+// the gird for where you paint
+const gridWidth = 10;
+let count = 0;
+while (count <= gridWidth * gridWidth) {
+  const canvas = document.querySelector('.canvas');
+  const div = document.createElement('div');
+  div.className = 'square color-5';
+  canvas.appendChild(div);
+  count++;
+}
+
+// evething thiat i will be targeting
+const canvasColor = document.querySelector('.current-brush')
+const canvas = document.querySelector('.canvas')
+const square = document.querySelector('.square')
+const palette = document.querySelector('.palette')
+const brush = document.querySelector('.current-brush.color-2')
+const curnentColor = document.querySelector('.icon palette-icon')
+const colorOne = document.querySelector('.palette-color color-1')//red
+const colorTwo = document.querySelector('.palette-color color-2')//black
+const colorThree = document.querySelector('.palette-color color-3')//blue
+const colorFour = document.querySelector('.palette-color color-4')//yellow
+const colorFive = document.querySelector('.palette-color color-5')//green
+
+
+// this section is for changing the color 
+
+function chagingActiveColor (event){
+  let PalletColor = event.target
+
+  let oldColor = brush.classList[1]
+
+  let NewColor = PalletColor.classList[1]
+
+  brush.classList.replace(oldColor, NewColor)
+
+}
+palette.addEventListener('click',chagingActiveColor)
+
+
+// this is to chage the color of the bruush selector  AKA update the color brush
+
+function LetsPaint (event){
+  let square = event.target
+
+
+  
+  let oldCavisColor = square.classList[1]
+
+  
+
+  let newCavisColor = canvasColor.classList[1]
+
+
+
+  square.classList.replace(oldCavisColor, newCavisColor)
+
+  
+
+}
+canvas.addEventListener('click',LetsPaint)
+// this will be chaging the grid color on clicks acording to the brush selector
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*******************
  * OUR HELPER CODE *
 *******************/
@@ -15,15 +112,6 @@
  * To make the second one happen, the number to change
  * is the first argument to `repeat`, currently set at 10.
  */
-const gridWidth = 10;
-let count = 0;
-while (count <= gridWidth * gridWidth) {
-  const canvas = document.querySelector('.canvas');
-  const div = document.createElement('div');
-  div.className = 'square color-5';
-  canvas.appendChild(div);
-  count++;
-}
 
 // You probably should NOT do these in the order below.
 // That is, you probably should NOT do all the queries,
